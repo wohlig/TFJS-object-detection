8245cf36fea7ba18599ee5a89202665d86c133ac
Chintan Shah <chintan@wohlig.com>
2021-03-06 22:06:18 +0530

    changes

---
+++ build/precache-manifest.05566b5e10d1726fa11b6d235375b5a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "924fff095b442466a885",
+    "url": "/static/css/main.412c2df6.chunk.css"
+  },
+  {
+    "revision": "924fff095b442466a885",
+    "url": "/static/js/main.924fff09.chunk.js"
+  },
+  {
+    "revision": "2ecd7810d566fdc1a1b0",
+    "url": "/static/js/1.2ecd7810.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "382ff11f242a4b19cce6fb5defd78eac",
+    "url": "/index.html"
+  }
+];
